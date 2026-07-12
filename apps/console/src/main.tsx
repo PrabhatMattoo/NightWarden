cbfc20283b7e13ffa9cb4f0f9f6093ae5752b02b
Flux690 <discordaryan@gmail.com>
2026-07-13 03:36:13 +0530

    feat(console): update font dependencies to Geist Sans - removed IBM Plex Sans

--- apps/console/src/main.tsx
+++ apps/console/src/main.tsx
@@ -1,9 +1,9 @@
-import "@fontsource/ibm-plex-sans/400.css";
-import "@fontsource/ibm-plex-sans/500.css";
-import "@fontsource/ibm-plex-sans/600.css";
-import "@fontsource/ibm-plex-sans/700.css";
-import "@fontsource/ibm-plex-mono/400.css";
-import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/geist-sans/400.css";
+import "@fontsource/geist-sans/500.css";
+import "@fontsource/geist-sans/600.css";
+import "@fontsource/geist-sans/700.css";
+import "@fontsource/geist-mono/400.css";
+import "@fontsource/geist-mono/500.css";
 import "./styles.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
